bb0c4a2d6d8cbc4e21d772ba4d1044d49efc0877
002CJE744 <002CJE744@LAPTOP-1T4DDRIK>
2021-09-22 15:38:35 +0530

    Add project files.

---
+++ ConsoleAppExceptionHandlingValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using classlibrary;
+
+namespace ConsoleAppExceptionHandlingValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 objBL = new Class1();
+            objBL.Name = Console.ReadLine();
+            objBL.Age = Int32.Parse(Console.ReadLine());
+
+            ValidationResults results = Validation.Validate(objBL);
+
+            if(!results.IsValid)
+            {
+                foreach (ValidationResult obj in results)
+                {
+                    Console.WriteLine(obj.Message);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
